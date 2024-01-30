909caa291a620bbc98e98345602d3d2c44986503
SrVieira <matheusvieirasto@gmail.com>
2024-01-30 16:20:35 -0300

    create resource to configure mysql

---
+++ resources/base/variables/server_variables.lua
@@ -0,0 +1,8 @@
+serverVariables = {};
+serverVariables.database = {
+    type = 'mysql',
+    host = '177.54.147.212',
+    name = 'matheusv1_2733',
+    username = 'matheusv1_2733',
+    password = '6UxOy12135',
+};
