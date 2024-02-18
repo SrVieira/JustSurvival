c3ba766e3644a51364e1acd2216e35c667f48382
SrVieira <matheusvieirasto@gmail.com>
2024-02-18 01:16:47 -0300

    atualizado o spawns handlers

---
+++ resources/[base]/Server/handlers/spawns_player.lua
@@ -0,0 +1,14 @@
+local random = math.random;
+
+local function getRandomSpawnByRegion(region)
+    return playerSpawnPositions[region][random(1, #playerSpawnPositions[region])];
+end
+
+-- Temp
+function handleCheckPlayerJoin()
+    local randomPos = getRandomSpawnByRegion("LOS SANTOS");
+    spawnPlayer(source, randomPos[1], randomPos[2], randomPos[3], 0, 0, 0, 0);
+    fadeCamera(source, true);
+    setCameraTarget(source, source);
+end
+addEventHandler("onPlayerJoin", getRootElement(), handleCheckPlayerJoin);
