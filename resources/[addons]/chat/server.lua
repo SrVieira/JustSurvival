9b7c7d3a0d86ec4d3faa91fd0e2fbb72ce7fd089
SrVieira <matheusvieirasto@gmail.com>
2024-02-20 12:32:41 -0300

    atualizado o onPlayerChat local

--- resources/[addons]/chat/server.lua
+++ resources/[addons]/chat/server.lua
@@ -6,3 +6,19 @@ function globalMessage(thePlayer, cmd, ...)
     outputChatBox("(GLOBAL) "..name..": "..message, getRootElement(), 255, 255, 255, true);
 end
 addCommandHandler("GlobalChat", globalMessage);
+
+function onPlayerChat(message, messageType)
+    cancelEvent();
+
+    local x, y, z = getElementPosition(source);
+    local players = getElementsByType("player");
+    
+    for _, player in ipairs(players) do
+        local px, py, pz = getElementPosition(player)
+        local distance = getDistanceBetweenPoints3D(x, y, z, px, py, pz);
+        if distance <= 10 then
+            outputChatBox("(LOCAL) "..getPlayerName(source)..": "..message, player);
+        end
+    end
+end
+addEventHandler("onPlayerChat", root, onPlayerChat);
