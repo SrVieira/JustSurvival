5f2dada8a749c1a4fea546ad2dea096d3671c459
SrVieira <matheusvieirasto@gmail.com>
2024-02-23 14:07:57 -0300

    add water bottle

--- resources/[models]/items/load.lua
+++ resources/[models]/items/load.lua
@@ -15,3 +15,9 @@ txd = engineLoadTXD("mountain_dew.txd", 2342);
 engineImportTXD(txd, 2342);
 dff = engineLoadDFF("mountain_dew.dff", 2342);
 engineReplaceModel(dff, 2342);
+
+-- Water Bottle
+txd = engineLoadTXD("water_bottle.txd", 2663);
+engineImportTXD(txd, 2663);
+dff = engineLoadDFF("water_bottle.dff", 2663);
+engineReplaceModel(dff, 2663);
