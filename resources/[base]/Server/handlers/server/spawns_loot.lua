edccec804071757158e1481a23f887f8bdee3f2d
SrVieira <matheusvieirasto@gmail.com>
2024-02-23 10:27:22 -0300

    criado objetos

--- resources/[base]/Server/handlers/server/spawns_loot.lua
+++ resources/[base]/Server/handlers/server/spawns_loot.lua
@@ -1,10 +1,21 @@
+local tableInsert = table.insert;
 lootPointsCols = {};
+
+local function createLootObjects(col, group)
+    local x, y, z = getElementPosition(col);
+
+    for k, loot in ipairs(lootItems[group]) do
+       createObject(loot[4], x, y, z);
+    end
+end
 
 local function createLootPoint(group)
     for k, point in ipairs(lootSpawnPoints[group]) do
         local col = createColSphere(point[1], point[2], point[3], 1.5);
         setElementData(col, "lootName", "Loot");
         setElementData(col, "maxSlots", point[4]);
+        local colObjects = createLootObjects(col, group);
+        tableInsert(lootPointsCols, col);
     end
 end
 
--- resources/[base]/Server/tables/loot_tables.lua
+++ resources/[base]/Server/tables/loot_tables.lua
@@ -1,7 +1,15 @@
 lootSpawnPoints = {
+    -- X, Y, Z, Loot Slots
     ["RESIDENTIAL"] = {
         { 2786.8322753906, -1926.2648925781, 12.546875, 8 },
         { 2786.8359375, -1952.7272949219, 12.546875, 8 },
         { 2792.0034179688, -1944.9205322266, 16.3203125, 8 },
+    }
+};
+
+lootItems = {
+    -- Name, Loot Size, Spawn Chance, Object, Object Size
+    ["RESIDENTIAL"] = {
+        { "Caixa de Leite", 1, 5, 14752, 1 },
     }
 };
