af68c3ee6461c3e5dd7b4bfcd4d2bc2347f04afa
SrVieira <matheusvieirasto@gmail.com>
2024-02-27 16:03:07 -0300

    atualizado o inventory

--- resources/[addons]/inventory/inventory_client.lua
+++ resources/[addons]/inventory/inventory_client.lua
@@ -44,11 +44,32 @@ local function renderInventoryGUI()
 	exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - 148, 232, 20, 1, {100, 100, 100, 225}, false);
     exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - 126, 232, 80, 1, {100, 100, 100, 125}, false);
     exports.dx:dxCustomDrawText('WTF WEAPON', centerX + 432, centerY - 148, centerX + 157 + 432, centerY - 148 + 22, tocolor(255, 255, 255, 200), 'SubTitleInventory', "left", "center");
+
+	-- Vest
+	exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - 41, 232, 20, 1, {100, 100, 100, 225}, false);
+	exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - 19, 232, 68, 1, {100, 100, 100, 125}, false);
+	exports.dx:dxCustomDrawText('UK ASSAULT VEST', centerX + 432, centerY - 41, centerX + 157 + 432, centerY - 41 + 22, tocolor(255, 255, 255, 200), 'SubTitleInventory', "left", "center");
+
+	-- Shirt
+	exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - (-54), 232, 20, 1, {100, 100, 100, 225}, false);
+	exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - (-76), 232, 68, 1, {100, 100, 100, 125}, false);
+	exports.dx:dxCustomDrawText('TACTICAL JACKET', centerX + 432, centerY - (-54), centerX + 157 + 432, centerY - (-54) + 22, tocolor(255, 255, 255, 200), 'SubTitleInventory', "left", "center");
+
+	-- Pants
+	exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - (-149), 232, 20, 1, {100, 100, 100, 225}, false);
+	exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - (-171), 232, 68, 1, {100, 100, 100, 125}, false);
+	exports.dx:dxCustomDrawText('BLUE PANTS', centerX + 432, centerY - (-149), centerX + 157 + 432, centerY - (-149) + 22, tocolor(255, 255, 255, 200), 'SubTitleInventory', "left", "center");
+
+	-- Backpack
+	exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - (-244), 232, 20, 1, {100, 100, 100, 225}, false);
+	exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - (-266), 232, 100, 1, {100, 100, 100, 125}, false);
+	exports.dx:dxCustomDrawText('MOCHILA DE ASSALTO', centerX + 432, centerY - (-244), centerX + 157 + 432, centerY - (-244) + 22, tocolor(255, 255, 255, 200), 'SubTitleInventory', "left", "center");
 end
 
 local function openCloseInventory()
 	if getElementData(localPlayer, "isLogged") and not getElementData(localPlayer, "isDead") then
 		inventory.visible = not inventory.visible
+		showCursor(inventory.visible);
 		if inventory.visible then
 			addEventHandler("onClientRender", getRootElement(), renderInventoryGUI);
 		else
