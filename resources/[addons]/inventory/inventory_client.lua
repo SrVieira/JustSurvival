81b308348fe6c862457901f251b572a44a41b116
SrVieira <matheusvieirasto@gmail.com>
2024-02-28 15:46:32 -0300

    update weapons

--- resources/[addons]/inventory/inventory_client.lua
+++ resources/[addons]/inventory/inventory_client.lua
@@ -1,6 +1,11 @@
 local screenW, screenH = guiGetScreenSize();
 local centerX, centerY = screenW/2, screenH/2;
 
+local renderTargetWidth, renderTargetHeight = 232, screenH - 70;
+local renderTargetX = (screenW - renderTargetWidth) - 30;
+local renderTargetY = (screenH - renderTargetHeight) / 2;
+local inventoryTarget = dxCreateRenderTarget(232, screenH, true);
+
 local inventory = {
     visible = false,
     header1 = 'Equipamento',
@@ -17,53 +22,66 @@ local function renderIconSlot(x, y, slotName)
 end
 
 local function renderInventoryGUI()
-	-- Equipaments
-    exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - 350, 232, 20, 1, {100, 100, 100, 225}, false);
-    exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - 328, 232, 68, 1, {100, 100, 100, 125}, false);
-    exports.dx:dxCustomDrawText(string.upper(inventory.header1), centerX + 432, centerY - 350, centerX + 157 + 432, centerY - 350 + 22, tocolor(255, 255, 255, 200), 'TitleInventory', "left", "center");
+	if inventoryTarget then
+		dxSetRenderTarget(inventoryTarget, true);
+		dxSetBlendMode("blend");
+		dxDrawRectangle(0, 0, renderTargetWidth, renderTargetHeight, tocolor(0, 0, 0, 0))
 
-	-- Slots
-	renderIconSlot(centerX + 432, centerY - 326, "helmet"); -- Helmet
-	renderIconSlot(centerX + 466, centerY - 326, "armor"); -- Armor
-	renderIconSlot(centerX + 500, centerY - 326, "mask"); -- Mask
-	renderIconSlot(centerX + 534, centerY - 326, "hand"); -- Hand
-	renderIconSlot(centerX + 568, centerY - 326, "shoes"); -- Shoes
-	renderIconSlot(centerX + 602, centerY - 326, "sword"); -- Sword
-	renderIconSlot(centerX + 432, centerY - 292, "radio"); -- Radio
-	renderIconSlot(centerX + 466, centerY - 292, "binoculars"); -- Binoculars
-	renderIconSlot(centerX + 500, centerY - 292, "gps"); -- GPS
-	renderIconSlot(centerX + 534, centerY - 292, "map"); -- Map
-	renderIconSlot(centerX + 568, centerY - 292, "compass"); -- Compass
+		-- Equipaments
+		exports.dx:dxDrawBorderedRectangle(0, 1, 232, 20, 1, {100, 100, 100, 225}, false);
+		exports.dx:dxDrawBorderedRectangle(0, 22, 232, 75, 1, {100, 100, 100, 125}, false);
+		exports.dx:dxCustomDrawText(string.upper(inventory.header1), 4, 0, 232, 22, tocolor(255, 255, 255, 255), 'TitleInventory', "left", "center");
 
-	-- Primary Weapon
-	exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - 255, 232, 20, 1, {100, 100, 100, 225}, false);
-    exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - 233, 232, 80, 1, {100, 100, 100, 125}, false);
-    exports.dx:dxCustomDrawText('SKS BLACK', centerX + 432, centerY - 255, centerX + 157 + 432, centerY - 255 + 22, tocolor(255, 255, 255, 200), 'SubTitleInventory', "left", "center");
+		-- Slots
+		renderIconSlot(4, 26, "helmet"); -- Helmet
+		renderIconSlot(40, 26, "armor"); -- Armor
+		renderIconSlot(76, 26, "mask"); -- Mask
+		renderIconSlot(112, 26, "hand"); -- Hand
+		renderIconSlot(148, 26, "shoes"); -- Shoes
+		renderIconSlot(184, 26, "sword"); -- Sword
 
-	-- Secondary Weapon
-	exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - 148, 232, 20, 1, {100, 100, 100, 225}, false);
-    exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - 126, 232, 80, 1, {100, 100, 100, 125}, false);
-    exports.dx:dxCustomDrawText('WTF WEAPON', centerX + 432, centerY - 148, centerX + 157 + 432, centerY - 148 + 22, tocolor(255, 255, 255, 200), 'SubTitleInventory', "left", "center");
+		renderIconSlot(4, 62, "radio"); -- Radio
+		renderIconSlot(40, 62, "binoculars"); -- Binoculars
+		renderIconSlot(76, 62, "gps"); -- GPS
+		renderIconSlot(112, 62, "map"); -- Map
+		renderIconSlot(148, 62, "compass"); -- Compass
 
-	-- Vest
-	exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - 41, 232, 20, 1, {100, 100, 100, 225}, false);
-	exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - 19, 232, 68, 1, {100, 100, 100, 125}, false);
-	exports.dx:dxCustomDrawText('UK ASSAULT VEST', centerX + 432, centerY - 41, centerX + 157 + 432, centerY - 41 + 22, tocolor(255, 255, 255, 200), 'SubTitleInventory', "left", "center");
+		-- Primary Weapon
+		exports.dx:dxDrawBorderedRectangle(0, 102, 232, 20, 1, {100, 100, 100, 225}, false);
+		exports.dx:dxDrawBorderedRectangle(0, 124, 232, 80, 1, {100, 100, 100, 125}, false);
+		exports.dx:dxCustomDrawText('SKS BLACK', 4, 102, 232, 124, tocolor(255, 255, 255, 255), 'SubTitleInventory', "left", "center");
 
-	-- Shirt
-	exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - (-54), 232, 20, 1, {100, 100, 100, 225}, false);
-	exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - (-76), 232, 68, 1, {100, 100, 100, 125}, false);
-	exports.dx:dxCustomDrawText('TACTICAL JACKET', centerX + 432, centerY - (-54), centerX + 157 + 432, centerY - (-54) + 22, tocolor(255, 255, 255, 200), 'SubTitleInventory', "left", "center");
+		-- Secondary Weapon
+		exports.dx:dxDrawBorderedRectangle(0, 209, 232, 20, 1, {100, 100, 100, 225}, false);
+		exports.dx:dxDrawBorderedRectangle(0, 231, 232, 80, 1, {100, 100, 100, 125}, false);
+		exports.dx:dxCustomDrawText('WTF WEAPON', 4, 209, 232, 231, tocolor(255, 255, 255, 255), 'SubTitleInventory', "left", "center");
 
-	-- Pants
-	exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - (-149), 232, 20, 1, {100, 100, 100, 225}, false);
-	exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - (-171), 232, 68, 1, {100, 100, 100, 125}, false);
-	exports.dx:dxCustomDrawText('BLUE PANTS', centerX + 432, centerY - (-149), centerX + 157 + 432, centerY - (-149) + 22, tocolor(255, 255, 255, 200), 'SubTitleInventory', "left", "center");
+		-- Vest
+		exports.dx:dxDrawBorderedRectangle(0, 316, 232, 20, 1, {100, 100, 100, 225}, false);
+		exports.dx:dxDrawBorderedRectangle(0, 338, 232, 68, 1, {100, 100, 100, 125}, false);
+		exports.dx:dxCustomDrawText('UK ASSAULT VEST', 4, 316, 232, 338, tocolor(255, 255, 255, 200), 'SubTitleInventory', "left", "center");
 
-	-- Backpack
-	exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - (-244), 232, 20, 1, {100, 100, 100, 225}, false);
-	exports.dx:dxDrawBorderedRectangle(centerX + 430, centerY - (-266), 232, 100, 1, {100, 100, 100, 125}, false);
-	exports.dx:dxCustomDrawText('MOCHILA DE ASSALTO', centerX + 432, centerY - (-244), centerX + 157 + 432, centerY - (-244) + 22, tocolor(255, 255, 255, 200), 'SubTitleInventory', "left", "center");
+		-- Shirt
+		exports.dx:dxDrawBorderedRectangle(0, 411, 232, 20, 1, {100, 100, 100, 225}, false);
+		exports.dx:dxDrawBorderedRectangle(0, 433, 232, 68, 1, {100, 100, 100, 125}, false);
+		exports.dx:dxCustomDrawText('TACTICAL JACKET', 4, 411, 232, 433, tocolor(255, 255, 255, 200), 'SubTitleInventory', "left", "center");
+
+		-- Pants
+		exports.dx:dxDrawBorderedRectangle(0, 506, 232, 20, 1, {100, 100, 100, 225}, false);
+		exports.dx:dxDrawBorderedRectangle(0, 528, 232, 68, 1, {100, 100, 100, 125}, false);
+		exports.dx:dxCustomDrawText('BLUE PANTS', 4, 506, 232, 528, tocolor(255, 255, 255, 200), 'SubTitleInventory', "left", "center");
+
+		-- Backpack
+		exports.dx:dxDrawBorderedRectangle(0, 601, 232, 20, 1, {100, 100, 100, 225}, false);
+		exports.dx:dxDrawBorderedRectangle(0, 623, 232, 120, 1, {100, 100, 100, 125}, false);
+		exports.dx:dxCustomDrawText('MOCHILA DE ASSALTO', 4, 601, 232, 623, tocolor(255, 255, 255, 200), 'SubTitleInventory', "left", "center");
+				
+		dxSetRenderTarget();
+	end
+end
+
+local function drawRenderTargetOnScreen()
+    dxDrawImage(renderTargetX, renderTargetY, renderTargetWidth, renderTargetHeight, inventoryTarget);
 end
 
 local function openCloseInventory()
@@ -72,8 +90,10 @@ local function openCloseInventory()
 		showCursor(inventory.visible);
 		if inventory.visible then
 			addEventHandler("onClientRender", getRootElement(), renderInventoryGUI);
+			addEventHandler("onClientRender", getRootElement(), drawRenderTargetOnScreen);
 		else
 			removeEventHandler("onClientRender", getRootElement(), renderInventoryGUI);
+			removeEventHandler("onClientRender", getRootElement(), drawRenderTargetOnScreen);
 		end
 	end
 end
